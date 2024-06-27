5045fbf3205ed31aa0e448c2f36b999b71f43403
pihaf <113176707+pihaf@users.noreply.github.com>
2024-06-27 14:43:10 +0700

    Feature/web_application_with_streamlit

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from levenshtein_distance import levenshtein_distance
+
+def load_vocab(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    words = sorted(set([line.strip().lower() for line in lines]))
+    return words
+
+def main():
+    vocabs = load_vocab(file_path='data/vocab.txt')
+    st.title("Word Correction using Levenshtein Distance")
+    word = st.text_input('Word:')
+
+    if st.button("Compute"):
+        # Compute levenshtein distance
+        leven_distances = dict()
+        for vocab in vocabs:
+            leven_distances[vocab] = levenshtein_distance(word, vocab)
+        
+        # Sorted by distance
+        sorted_distences = dict(sorted(leven_distances.items(), key=lambda item: item[1]))
+        correct_word = list(sorted_distences.keys())[0]
+        st.write('Correct word: ', correct_word)
+
+        col1, col2 = st.columns(2)
+        col1.write('Vocabulary:')
+        col1.write(vocabs)
+        
+        col2.write('Distances:')
+        col2.write(sorted_distences)
+
+if __name__ == "__main__":
+    main()
--- src/test/test_all.py
+++ src/test/test_all.py
@@ -1,4 +1,10 @@
 from levenshtein_distance import levenshtein_distance
+from main import load_vocab
 
 def test_levenshtein_distance():
     assert levenshtein_distance('hola', 'hello') == 3
+
+def test_load_vocab():
+    vocabs = load_vocab(file_path='../data/vocab.txt')
+    assert len(vocabs) > 0
+    assert all(isinstance(word, str) for word in vocabs)
